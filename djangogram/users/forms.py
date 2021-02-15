117fcdb1ae980634a2028928de843986fed0d801
wnsals411 <wnsals411@naver.com>
2021-02-16 04:42:44 +0900

    5-2 까지 완료

--- djangogram/users/forms.py
+++ djangogram/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django import forms as django_forms
 
 User = get_user_model()
 
@@ -17,3 +18,25 @@ class UserCreationForm(admin_forms.UserCreationForm):
         error_messages = {
             "username": {"unique": _("This username has already been taken.")}
         }
+
+class SignUpForm(django_forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+        labels = {
+            'email': '이메일 주소',
+            'name': '성명',
+            'username': '사용자 이름',
+            'password': '비밀번호',
+        }
+        widgets = {
+            'password': django_forms.PasswordInput(),
+        }           
+        
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- djangogram/users/urls.py
+++ djangogram/users/urls.py
@@ -4,4 +4,5 @@ from djangogram.users import views
 app_name = "users"
 urlpatterns = [
     path('', views.main, name='main'),
+    path('signup/', views.signup, name='signup'),
 ]
